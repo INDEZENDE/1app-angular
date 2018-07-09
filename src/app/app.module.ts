51f6e3a5c045ef34838272c31ef0389b150104bc
INDEZENDE <i.rios.zendejas@gmail.com>
2018-07-08 22:50:07 -0500

    router con material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { N2Component } from './n2/n2.component';
+import { N3Component } from './n3/n3.component';
+import { N1Component } from './n1/n1.component';
+
+
+const appRoutes: Routes = [
+  {path:'news1', component: N1Component}
+  {path:'news2', component:N2Component},
+  {path:'news3', component: N3Component}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    N2Component,
+    N3Component,
+    N1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/n1/n1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-n1',
+  templateUrl: './n1.component.html',
+  styleUrls: ['./n1.component.css']
+})
+export class N1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
