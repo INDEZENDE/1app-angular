eb854b716640613d85b10d6987d7708b44384e09
INDEZENDE <i.rios.zendejas@gmail.com>
2018-07-09 19:56:33 -0500

    Error correjido

---
+++ src/app/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+// @Injectable()
+
+// export class AppService {
+
+//   private readonly API='http://node-hnapi.herokuapp.com/news';
+//   constructor(private http: HttpClient) { }
+
+//   getOrganization(name: string): Observable <any>{
+//     return this.http.get( URL: 'http://node-hnapi.herokuapp.com/news');
+//   }
+// }
